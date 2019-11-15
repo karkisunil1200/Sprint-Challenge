a3e21ac14eb739647546685e20242b69177b08b2
Sunil Karki <karkisunil1200@gmail.com>
2019-11-15 10:59:17 -0600

    Added searchBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
 import Header from './components/Header.js';
 import CharacterList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
+import SearchForm from './components/SearchForm';
 
 export default function App() {
   return (
     <main>
       <Header />
+
+      <Route path='/' component={WelcomePage} />
+
+      <SearchForm />
       <CharacterList />
     </main>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 
 export default function SearchForm() {
- 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+
+  const changeHandler = e => {
+    setSearchTerm({[e.target.name]: e.target.value});
+  };
   return (
-    <section className="search-form">
-     // Add a search form here
+    <section className='search-form'>
+      <h3>Search for the name!</h3>
+      <form>
+        <input
+          type='text'
+          name='search'
+          placeholder='Search...'
+          onChange={changeHandler}
+          value={searchTerm}
+        />
+      </form>
     </section>
   );
 }

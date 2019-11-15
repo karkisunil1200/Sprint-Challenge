82aabe27f94d7a59c3cd8df94c4df558b6db10a6
Sunil Karki <karkisunil1200@gmail.com>
2019-11-15 10:04:31 -0600

    Starting project

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  useEffect(() => {}, []);
 
   return (
-    <section className="character-list">
+    <section className='character-list'>
       <h2>TODO: `array.map()` over your state here!</h2>
     </section>
   );

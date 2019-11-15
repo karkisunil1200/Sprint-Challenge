dd5c3b1cd5bdf5f4b16ad1e286c927119a873309
Sunil Karki <karkisunil1200@gmail.com>
2019-11-15 10:11:27 -0600

    Data rendering

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,14 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+import CharacterCard from './CharacterCard';
+
 export default function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+
   // TODO: Add useState to track data from useEffect
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        console.log(res.data.results);
+        setCharacters(res.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <section className='character-list'>
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => {
+        return <CharacterCard character={character} />;
+      })}
     </section>
   );
 }
